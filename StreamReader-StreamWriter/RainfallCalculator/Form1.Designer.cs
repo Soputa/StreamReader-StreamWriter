b029c9dae7e53760ef0d57e171b074faac293351
Sir Oputa <80017134+Soputa@users.noreply.github.com>
2021-12-28 23:22:14 -0500

    Add files via upload

---
+++ StreamReader-StreamWriter/RainfallCalculator/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace RainfallCalculator
+{
+    partial class Rainfall_Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelValidRows = new System.Windows.Forms.Label();
+            this.labelTotalRows = new System.Windows.Forms.Label();
+            this.labelHighestValue = new System.Windows.Forms.Label();
+            this.labelLowestValue = new System.Windows.Forms.Label();
+            this.labelAverageRainfall = new System.Windows.Forms.Label();
+            this.buttonImport = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.textBoxTotalRows = new System.Windows.Forms.TextBox();
+            this.textBoxHighestValue = new System.Windows.Forms.TextBox();
+            this.textBoxLowestValue = new System.Windows.Forms.TextBox();
+            this.textBoxAverageRainfall = new System.Windows.Forms.TextBox();
+            this.textBoxValidRows = new System.Windows.Forms.TextBox();
+            this.buttonSaveFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelValidRows
+            // 
+            this.labelValidRows.AutoSize = true;
+            this.labelValidRows.Location = new System.Drawing.Point(66, 128);
+            this.labelValidRows.Name = "labelValidRows";
+            this.labelValidRows.Size = new System.Drawing.Size(63, 13);
+            this.labelValidRows.TabIndex = 1;
+            this.labelValidRows.Text = "Valid Rows:";
+            // 
+            // labelTotalRows
+            // 
+            this.labelTotalRows.AutoSize = true;
+            this.labelTotalRows.Location = new System.Drawing.Point(66, 185);
+            this.labelTotalRows.Name = "labelTotalRows";
+            this.labelTotalRows.Size = new System.Drawing.Size(64, 13);
+            this.labelTotalRows.TabIndex = 2;
+            this.labelTotalRows.Text = "Total Rows:";
+            // 
+            // labelHighestValue
+            // 
+            this.labelHighestValue.AutoSize = true;
+            this.labelHighestValue.Location = new System.Drawing.Point(66, 238);
+            this.labelHighestValue.Name = "labelHighestValue";
+            this.labelHighestValue.Size = new System.Drawing.Size(76, 13);
+            this.labelHighestValue.TabIndex = 3;
+            this.labelHighestValue.Text = "Highest Value:";
+            // 
+            // labelLowestValue
+            // 
+            this.labelLowestValue.AutoSize = true;
+            this.labelLowestValue.Location = new System.Drawing.Point(66, 283);
+            this.labelLowestValue.Name = "labelLowestValue";
+            this.labelLowestValue.Size = new System.Drawing.Size(74, 13);
+            this.labelLowestValue.TabIndex = 4;
+            this.labelLowestValue.Text = "Lowest Value:";
+            // 
+            // labelAverageRainfall
+            // 
+            this.labelAverageRainfall.AutoSize = true;
+            this.labelAverageRainfall.Location = new System.Drawing.Point(66, 327);
+            this.labelAverageRainfall.Name = "labelAverageRainfall";
+            this.labelAverageRainfall.Size = new System.Drawing.Size(88, 13);
+            this.labelAverageRainfall.TabIndex = 5;
+            this.labelAverageRainfall.Text = "Average Rainfall:";
+            // 
+            // buttonImport
+            // 
+            this.buttonImport.Location = new System.Drawing.Point(106, 390);
+            this.buttonImport.Name = "buttonImport";
+            this.buttonImport.Size = new System.Drawing.Size(75, 23);
+            this.buttonImport.TabIndex = 6;
+            this.buttonImport.Text = "Import";
+            this.buttonImport.UseVisualStyleBackColor = true;
+            this.buttonImport.Click += new System.EventHandler(this.buttonImport_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(419, 390);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 7;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // textBoxTotalRows
+            // 
+            this.textBoxTotalRows.Location = new System.Drawing.Point(170, 185);
+            this.textBoxTotalRows.Name = "textBoxTotalRows";
+            this.textBoxTotalRows.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTotalRows.TabIndex = 9;
+            // 
+            // textBoxHighestValue
+            // 
+            this.textBoxHighestValue.Location = new System.Drawing.Point(170, 238);
+            this.textBoxHighestValue.Name = "textBoxHighestValue";
+            this.textBoxHighestValue.Size = new System.Drawing.Size(100, 20);
+            this.textBoxHighestValue.TabIndex = 10;
+            // 
+            // textBoxLowestValue
+            // 
+            this.textBoxLowestValue.Location = new System.Drawing.Point(170, 283);
+            this.textBoxLowestValue.Name = "textBoxLowestValue";
+            this.textBoxLowestValue.Size = new System.Drawing.Size(100, 20);
+            this.textBoxLowestValue.TabIndex = 11;
+            // 
+            // textBoxAverageRainfall
+            // 
+            this.textBoxAverageRainfall.Location = new System.Drawing.Point(170, 327);
+            this.textBoxAverageRainfall.Name = "textBoxAverageRainfall";
+            this.textBoxAverageRainfall.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAverageRainfall.TabIndex = 12;
+            // 
+            // textBoxValidRows
+            // 
+            this.textBoxValidRows.Location = new System.Drawing.Point(170, 128);
+            this.textBoxValidRows.Name = "textBoxValidRows";
+            this.textBoxValidRows.Size = new System.Drawing.Size(100, 20);
+            this.textBoxValidRows.TabIndex = 13;
+            // 
+            // buttonSaveFile
+            // 
+            this.buttonSaveFile.Location = new System.Drawing.Point(270, 390);
+            this.buttonSaveFile.Name = "buttonSaveFile";
+            this.buttonSaveFile.Size = new System.Drawing.Size(75, 23);
+            this.buttonSaveFile.TabIndex = 14;
+            this.buttonSaveFile.Text = "Save";
+            this.buttonSaveFile.UseVisualStyleBackColor = true;
+            this.buttonSaveFile.Click += new System.EventHandler(this.buttonSaveFile_Click);
+            // 
+            // Rainfall_Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSaveFile);
+            this.Controls.Add(this.textBoxValidRows);
+            this.Controls.Add(this.textBoxAverageRainfall);
+            this.Controls.Add(this.textBoxLowestValue);
+            this.Controls.Add(this.textBoxHighestValue);
+            this.Controls.Add(this.textBoxTotalRows);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonImport);
+            this.Controls.Add(this.labelAverageRainfall);
+            this.Controls.Add(this.labelLowestValue);
+            this.Controls.Add(this.labelHighestValue);
+            this.Controls.Add(this.labelTotalRows);
+            this.Controls.Add(this.labelValidRows);
+            this.Name = "Rainfall_Calculator";
+            this.Text = "Rainfall Calculator";
+            this.Load += new System.EventHandler(this.Rainfall_Calculator_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label labelValidRows;
+        private System.Windows.Forms.Label labelTotalRows;
+        private System.Windows.Forms.Label labelHighestValue;
+        private System.Windows.Forms.Label labelLowestValue;
+        private System.Windows.Forms.Label labelAverageRainfall;
+        private System.Windows.Forms.Button buttonImport;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.TextBox textBoxTotalRows;
+        private System.Windows.Forms.TextBox textBoxHighestValue;
+        private System.Windows.Forms.TextBox textBoxLowestValue;
+        private System.Windows.Forms.TextBox textBoxAverageRainfall;
+        private System.Windows.Forms.TextBox textBoxValidRows;
+        private System.Windows.Forms.Button buttonSaveFile;
+    }
+}
+
---
+++ StreamReader-StreamWriter/RainfallCalculator/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;                  
+
+
+namespace RainfallCalculator
+{
+   
+    public partial class Rainfall_Calculator : Form
+    {
+        
+        protected int TextBoxCount { get; set; } = 5;
+
+            public enum TextBoxIndices { Valid,TotalRows,Highest,Lowest,Average}
+        const string folderPath = @"C:\test\rainfall.txt";
+        string rainNew = @"C:\Users\SirOp\Desktop\test\rainfall2.txt";
+        //Querry files in the given directory
+        //string[] listOfFiles = Directory.GetFiles(folderPath);
+
+        //reads data from our rainfall txt file
+       
+        private const string RainFallSenValue = "0.474452";
+
+        private List<double> RainfallData { get; set; }
+        public Rainfall_Calculator()
+        {
+            InitializeComponent();
+
+            //Clear textboxes
+            textBoxHighestValue.Text = "";
+            textBoxLowestValue.Text = ""; 
+            textBoxAverageRainfall.Text ="";
+            textBoxValidRows.Text = "";
+            textBoxTotalRows.Text = "";
+            buttonSaveFile.Enabled = false;
+            //Craeate RainfallData list
+            RainfallData = new List<double>();
+            
+        }
+
+        public void ClearTextBoxes()
+        {
+            //iterate through every Control on form and clears it
+            foreach(Control guiControl in Controls)
+            {
+                (guiControl as TextBox)?.Clear();
+            }
+                
+        }
+
+        /* public void SetTextBoxValues(string[] values)
+         {
+             if(values.Length !=TextBoxCount)
+             {
+                 throw (new ArgumentException($"There must be {TextBoxCount} strings in the array", nameof(values)));
+             }
+             else //array values at 5 
+             {
+                 //set the array values
+                 textBoxValidRows.Text = values[(int) TextBoxIndices.Valid];
+                 textBoxTotalRows.Text = values[(int)TextBoxIndices.TotalRows];
+                 textBoxHighestValue.Text = values[(int)TextBoxIndices.Highest];
+                 textBoxLowestValue.Text = values[(int)TextBoxIndices.Lowest];
+                 textBoxAverageRainfall.Text = values[(int)TextBoxIndices.Average];
+
+             }
+
+         }
+
+         //return TextBox values as string array
+         public string[] GetTextBoxValues()
+         {
+             return new string[] {
+
+                 textBoxValidRows.Text,textBoxTotalRows.Text,textBoxHighestValue.Text,textBoxLowestValue.Text,textBoxAverageRainfall.Text}; 
+
+         }
+        */
+        //public long CountLinesLINQ(FileInfo )
+        
+       
+        private void buttonImport_Click(object sender, EventArgs e)
+        {
+            //Create & show a dialog box enabling user to import their file
+            DialogResult result;
+            string fileName;
+
+            using (OpenFileDialog fileChooser = new OpenFileDialog())
+            {
+                result = fileChooser.ShowDialog();
+                fileName = fileChooser.FileName; // get the file name(rainfall.txt)
+               
+            }
+
+            //IF yes
+            if(result == DialogResult.OK)
+            {
+                ClearTextBoxes();
+
+                //error if file selected is invalid
+                if (!File.Exists(fileName))
+                {
+                    MessageBox.Show("Invalid File Name", "Please Select rainfall.txt", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {   
+
+                    try
+                    {
+                        FileStream inputfile = File.Open(fileName, FileMode.Open, FileAccess.Read);
+                        StreamReader reader = new StreamReader(inputfile);
+                        string latestLine = "";
+                        RainfallData.Clear();
+                        int totalLinesProcessed = 0;
+
+                        //Keep looping until we reach the sentinel
+                        while ((!reader.EndOfStream) && (latestLine != RainFallSenValue))
+                        {
+                            //Read next line of file
+                            latestLine = reader.ReadLine();
+                            //Count it if it's not sentinel value
+                            if (latestLine != RainFallSenValue)
+                            {
+                                totalLinesProcessed++;
+                            }
+
+                            if (IsGoodRainfall(latestLine, out double rainfallValue))
+                                RainfallData.Add(rainfallValue);
+                        }
+                            //Close up the streams
+                            reader.Close();
+                            inputfile.Close();
+
+                            
+                            
+
+                            //Largest number
+                            textBoxHighestValue.Text = $"{RainfallData.Max():n0}";
+                            //smallest number
+                            textBoxLowestValue.Text = $"{RainfallData.Min():n0}";
+                            //Avarage of all rows
+                            textBoxAverageRainfall.Text = $"{RainfallData.Average():n0}";
+                            //Count of all rows
+                            textBoxValidRows.Text = $"{RainfallData.Count():n0}";
+                            //Valid Rows
+                            textBoxTotalRows.Text = $"{totalLinesProcessed:n0}";
+
+                            buttonSaveFile.Enabled = true;
+
+                        
+                    }
+                    catch (IOException)
+                        {
+                        MessageBox.Show("Error reading from file", "File Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                }
+            }
+
+
+        }
+        
+        private bool IsGoodRainfall(string inputLine, out double numericValue)
+        {
+            bool retValue;
+        double numericCandidate =-1;
+
+        if (inputLine == RainFallSenValue)
+            retValue = false;
+        else if (!double.TryParse(inputLine, out numericCandidate))
+            retValue = false;
+        else if (numericCandidate < 0.1)
+            retValue = false;
+        else
+            retValue = true; //All good pass
+
+        numericValue = numericCandidate;
+        return retValue;
+
+        }
+
+        private void Rainfall_Calculator_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            DialogResult answer = MessageBox.Show("Are you sure you want to exit?", "Exit the application", MessageBoxButtons.YesNo);
+            if (answer == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+
+        private void buttonSaveFile_Click(object sender, EventArgs e)
+        {   
+            
+            bool moveAhead = true;
+          //CHeck to see if the output file exist 
+          //see if the user is ok to overwrite it
+            if(File.Exists(rainNew))
+            {
+                moveAhead = (MessageBox.Show("The outputFile exist. Overwrite it?", "Are you sure you want to Overwrite the FIle?", MessageBoxButtons.YesNo) == DialogResult.Yes);
+            }
+
+            if(moveAhead)
+            {
+                
+                //DialogResult result1;
+                //fileName = fileChooser.FileName;
+
+               
+                try
+                {
+
+                    //Save the good rows to the file 
+                    //I tried a few ways but, unable to save the file 
+                    FileStream outFile = File.Open(rainNew, FileMode. Create, FileAccess.Write);
+                    StreamWriter writer = new StreamWriter(outFile);
+                   
+                    
+                        //Loop through all of the good rows in the list
+                        foreach (double rainfallValue in RainfallData)
+                            writer.WriteLine($"{rainfallValue:n8}");
+
+                        //Tiddy up close streams
+                        writer.Close();
+                        outFile.Close();
+                    
+                }
+
+                catch(IOException)
+                {
+
+                    MessageBox.Show("Error saving the file", "File Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+
+
+            }
+            MessageBox.Show("Nice!: The valid rows have been written to the output file.");
+
+        }
+
+    }    
+    }
+
+                                                                                                                                                                                                              
